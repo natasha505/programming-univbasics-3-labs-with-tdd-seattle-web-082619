f08eddd4e3a3e8ea7fba2f3aba1f11a73041d8c3
Natali kruk <natasha_505@yahoo.com>
2019-08-20 21:57:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,7 @@
 # Add your variables here
 
-def calculator do
-  first_number = 5.to_f 
-  second_number = 2.to_f 
-  sum = first_number + second_number
+
+first_number = 5.to_f 
+second_number = 2.to_f 
+sum = first_number + second_number
   
-end 
