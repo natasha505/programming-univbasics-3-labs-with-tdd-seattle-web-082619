1c5291c8da193b7b185bbc94b41f7a5b3332b089
Natali kruk <natasha_505@yahoo.com>
2019-08-20 21:53:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+
+def calculator do
+  first_number = num1.gets.chomp().to_f 
+  second_number = num2.gets.chomp().to_f && != 0 
+  sum = first_number + second_number
+  
+end 
